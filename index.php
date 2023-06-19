93b7c5ca368aed5906d9054f4e0aed80cb725225
Steve Grard <grard.steve@gmail.com>
2023-06-19 09:37:12 +0200

    Bases of work ready

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php require "./views/partials/header.php" ?>
+
+    <h1 class="text-center text-4xl mt-5 mb-5">Hackers poulette</h1>
+
+    <figure class="mt-5 mb-10">
+        <img class="w-48 h-48 mx-auto" src="./assets/img/poulette.png" alt="Image of a beautifull 'poulette'">
+        <figcaption class="text-center italic">Coât ?</figcaption>
+    </figure>
+<?php require "./views/partials/footer.php" ?>
